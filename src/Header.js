3365351acfd063206a2bceeef6824ef29e1e1fa6
Batool Kazmi <kazmib6@gmail.com>
2024-05-04 19:11:21 -0400

    Adding Css for the header component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 // The Header Component -- file is capitalized because it is an component
-import React from 'react'
+import React from 'react';
+import "./Header.css";
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
 
 function Header() {
     return (
-        <div>
+        // BEM <<<<
+        <div className='header'>
             <PersonIcon />
             <h2>I am a header</h2>
             <MessageIcon />
