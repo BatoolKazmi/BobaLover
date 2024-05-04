cf5c9cc271165599bbcc960e7aac541a314e79fe
Batool Kazmi <kazmib6@gmail.com>
2024-05-04 18:49:07 -0400

    Writing out the first function component (how to write default function component)

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// The Header Component -- file is capitalized because it is an component
+import React from 'react'
+
+function Headedr() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Header
