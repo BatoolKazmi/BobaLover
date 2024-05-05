0ba5d415f195d57abf458a2eedf1db4aaf3d00ce
Batool Kazmi <kazmib6@gmail.com>
2024-05-04 21:00:16 -0400

    figured out v6 react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+
+// react-router-dom v6 (new version 2024)
+// Switch is replaced by routes "Routes"
+// <Route path="/" component={Home} /> to <Route path='/' element={<Home/>} />
+
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Link
 } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <h1>BOBATINDER!</h1> */}
-
-        {/* Header */}
-        <Header />
-
-        {/* Tinder Cards */}
-        {/* Buttons below Tinder cards */}
-
-        {/* Chats screen (top right corner) */}
-        {/* Individual chat screen  */}
+        <Routes>
+          <Route path="/chat" element={<h1>I am the chatpage</h1>} />
+          <Route path="/" element={<h1>I am Homepage</h1>} />
+        </Routes>
       </Router>
     </div>
   );
 }
+
+{/* Header */ }
+{/* <Header /> */ }
+{/* Tinder Cards */ }
+{/* Buttons below Tinder cards */ }
+
+{/* Chats screen (top right corner) */ }
+{/* Individual chat screen  */ }
 
 export default App;
