77bdc434c5db1205d55b536c3b19cbbcf2234eec
Batool Kazmi <kazmib6@gmail.com>
2024-05-05 09:34:46 -0400

    added variables to index.css (the root)

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,8 +1,9 @@
 // The Header Component -- file is capitalized because it is an component
 import React, { useState } from "react";
 // imported the TinderCard that we installed earlier
-import TinderCard from 'react-tinder-card'
-
+import TinderCard from 'react-tinder-card';
+import "./TinderCards.css";
+import BoboTea from './imgs/BoboTea.png';
 
 // Can use function or ES6 arrow function
 function TinderCards() {
@@ -10,14 +11,15 @@ function TinderCards() {
     // people called & modifier is setPeople
     // setPeople([...people, 'sony', 'qazi']) -- way of pushing (spread)
 
+    // Can get Images of Bubble tea from diffrent shops from Uber eats
     const [people, setPeople] = useState([
         {
-            name: "steve jobs",
-            url: "https://th.bing.com/th/id/OIP.OJoSFHphzK66ytsLTI15WwHaH0?rs=1&pid=ImgDetMain"
+            name: "BoboTea Strawberry",
+            url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ubereats.com%2Fca%2Fstore%2Fbobo-tea%2FPi3rmpMxWJ2LQpFsDjGlcQ&psig=AOvVaw1z1OzovoetfLzZxQ3GSvKH&ust=1715000508264000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiUi_PI9oUDFQAAAAAdAAAAABAl",
         },
         {
-            name: "markum markum",
-            url: "https://thumbs.dreamstime.com/z/face-successful-person-closeup-young-businessman-blurred-background-office-104046254.jpg"
+            name: "Chatime Standard Milk Tea",
+            url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.skipthedishes.com%2Fchatime-bedford&psig=AOvVaw0RiQkQr-bV2prz-cCBaIyg&ust=1715000412298000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLix6cbI9oUDFQAAAAAdAAAAABAW"
         },
     ]);
     // Same as typing => const people = []
@@ -25,19 +27,24 @@ function TinderCards() {
     return (
         <div>
             <h1>Tinder Cards</h1>
+            <imgs src={BoboTea} alt="" />
 
-            {people.map(person => (
-                <TinderCard className="swipe"
-                    key={person.name}
-                    preventSwipe={['up', 'down']}
-                >
-                    <div
-                        style={{ backgroundImage: `url(${person.url})` }}
-                        className='card'>
-                        <h3>{person.name}</h3>
-                    </div>
-                </TinderCard>
-            ))}
+            <div class="tinderCards_cardContainer">
+                {people.map(person => (
+                    <TinderCard className="swipe"
+                        // Key very important in React
+                        key={person.name}
+                        // Prevent from swiping up and down
+                        preventSwipe={['up', 'down']}
+                    >
+                        <div
+                            style={{ backgroundImage: `url(${person.url})` }}
+                            className='card'>
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     );
 }
