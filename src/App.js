03118c6f4d4be7b7436f08127469f3ed5d56d106
Batool Kazmi <kazmib6@gmail.com>
2024-05-04 20:14:13 -0400

    installing and adding router

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <h1>BOBATINDER!</h1> */}
+      <Router>
+        {/* <h1>BOBATINDER!</h1> */}
 
-      {/* Header */}
-      <Header />
-      {/* Tinder Cards */}
-      {/* Buttons below Tinder cards */}
+        {/* Header */}
+        <Header />
 
-      {/* Chats screen (top right corner) */}
-      {/* Individual chat screen  */}
+        {/* Tinder Cards */}
+        {/* Buttons below Tinder cards */}
 
+        {/* Chats screen (top right corner) */}
+        {/* Individual chat screen  */}
+      </Router>
     </div>
   );
 }
