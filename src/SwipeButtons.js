f8fdea0cbf36dffff4852b80ebd6e92d68f1c3de
Batool Kazmi <kazmib6@gmail.com>
2024-05-12 14:16:18 -0400

    added buttons to Swipe Buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,16 +1,24 @@
-// The Header Component -- file is capitalized because it is an component
 import React from 'react';
-import "./SwipeButtons.css"
+import "./SwipeButtons.css";
+
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarIcon from '@mui/icons-material/Star';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
 
 
-// Can use function or ES6 arrow function
 function SwipeButtons() {
     return (
-        // BEM <<<<
         <div>
-            <h1>Buttons Here</h1>
+            <h1>Buttons Go Here</h1>
+            <ReplayIcon />
+            <CloseIcon />
+            <StarIcon />
+            <FavoriteIcon />
+            <FlashOnIcon />
         </div>
     );
-}
+};
 
 export default SwipeButtons;
