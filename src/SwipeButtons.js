fcbab0d5c81a134c7371a7c30f67a295ecadc26f
Batool Kazmi <kazmib6@gmail.com>
2024-05-12 14:54:42 -0400

    styled the buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -6,17 +6,27 @@ import CloseIcon from '@mui/icons-material/Close';
 import StarIcon from '@mui/icons-material/Star';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { IconButton } from '@mui/material';
 
 
 function SwipeButtons() {
     return (
         <div className='swipeButtons'>
-            <h1>Buttons Go Here</h1>
-            <ReplayIcon />
-            <CloseIcon />
-            <StarIcon />
-            <FavoriteIcon />
-            <FlashOnIcon />
+            <IconButton className='repeat'>
+                <ReplayIcon />
+            </IconButton>
+            <IconButton className='left'>
+                <CloseIcon />
+            </IconButton>
+            <IconButton className='star'>
+                <StarIcon />
+            </IconButton>
+            <IconButton className='right'>
+                <FavoriteIcon />
+            </IconButton>
+            <IconButton className='lightning'>
+                <FlashOnIcon />
+            </IconButton>
         </div>
     );
 };
